f05f6372c51d87846b4e00147a56b820974c325b
Wejden Belhadj Brahim <wejdenn1@gmail.com>
2024-06-02 22:21:27 +0100

    fix: change `home pages` images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeimage from "../../Assets/homeimage.PNG";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name">  WEJDEN BELHADJ BRAHIM</strong>
+                <strong className="main-name"> WEJDEN BELHADJ BRAHIM</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -32,7 +32,7 @@ function Home() {
 
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
-                src={homeLogo}
+                src={homeimage}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import women from "../../Assets/women.PNG";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub
-} from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
@@ -44,7 +42,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={women} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>

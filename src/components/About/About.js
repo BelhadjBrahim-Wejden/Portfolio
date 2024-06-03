a20a255c8af43563b71efbfd0c9589df97343737
Wejden Belhadj Brahim <wejdenn1@gmail.com>
2024-06-03 19:51:08 +0100

    refactor: change `about ` image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+import laptopImg from "../../Assets/about1.png";
 import Toolstack from "./Toolstack";
 
 function About() {
